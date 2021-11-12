04e149676560191bace1141bcb0a1c0687330025
Yuliya Yushkova <kuroi.kasa1509@gmail.com>
2021-11-12 14:45:38 +0700

    Npm publish action update react native (#66)
    * Update: main.yml file update to compile .js files on build

--- compile/src/MMKVStorage.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// @ts-ignore
-const react_native_mmkv_1 = require("react-native-mmkv");
-const storage = new react_native_mmkv_1.MMKV();
-//TODO: add getData management based on prop type
-exports.default = storage;
